787daace1ac70de787eda828bb173e2a2e79e4c2
Aditya Sharma <rahulsharma@rahuls-MacBook-Air.local>
2026-04-24 13:41:00 +0530

    update: syntex in 01-problem

--- array-hashing/01-contain-duplicate.cpp
+++ array-hashing/01-contain-duplicate.cpp
@@ -2,20 +2,19 @@
 #include <vector>
 #include <unordered_set>
 using namespace std;
-bool containduplicate(vector<int>&nums){
-        unordered_set<int> s;
-        for (size_t i = 0; i < nums.size(); i++){
-            if (s.count(nums[i])){
+bool containduplicate(vector<int> &nums){
+    unordered_set<int> s;
+    for (int x : nums) {
+        if (s.count(x)) {
             return true;
-
-            }
-             s.insert(nums[i]);
-      }
-           
-          return false;
+        }
+        s.insert(x);
+    }
+    return false;
 }
-int main(){
-    vector<int>nums = {1,2,3,3};
-   cout << containduplicate(nums);
-   return 0;
+int main()
+{
+    vector<int> nums = {1, 2, 3, 3};
+    cout << containduplicate(nums);
+    return 0;
 }
---
+++ array-hashing/04-Top-k-frequent-Elements.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+int main(){
+    vector<int> arr = {1,1,2,2,2,3,3,4,5};
+    int k = 3;  
+}
